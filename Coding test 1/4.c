d9d87f54eb8e933102f473c5d04d6606e159c763
Sazzadul haque <66717227+1834902552@users.noreply.github.com>
2020-08-08 12:18:54 +0600

    Add files via upload

---
+++ Coding test 1/4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main()
+{
+    int arr[100];
+    int i=0;
+    int size;
+    int value,index;
+
+    printf("size of the array : ");
+    scanf("%d",&size);
+
+
+    printf("Enter values in array : ");
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    printf("value to insert : ");
+    scanf("%d",&value);
+    printf("value of index : ");
+    scanf("%d",&index);
+
+    if(index > size+1 || index <= 0)
+    {
+        printf("Invalid index! Please enter index between 1 to %d", size);
+    }
+    else
+    {
+        for(i=size; i>=index; i--)
+        {
+            arr[i] = arr[i-1];
+        }
+        arr[index-1] = value;
+        size++;
+        printf("Array values after insertion : ");
+        for(i=0; i<size; i++)
+        {
+            printf("%d\t", arr[i]);
+        }
+    }
+
+    return 0;
+}
